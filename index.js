f8fd1d4ccb155101f29bdbb888b66f82e7772b6c
Kevin <kevin.mavier@hotmail.com>
2024-11-26 13:51:55 +0100

    add payment routes

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
+const cloudinary = require("cloudinary").v2;
 
 require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 mongoose.connect(process.env.MONGODB_URI);
+cloudinary.config({
+    cloud_name: "lereacteur",
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
 const usersRoutes = require("./routes/user");
 app.use('/users',usersRoutes)
 
 const offersRoutes = require("./routes/offer");
 app.use('/offers',offersRoutes)
+
+const paymentsRoutes = require("./routes/payment");
+app.use('/payments',paymentsRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log("Server started on port 3000");
